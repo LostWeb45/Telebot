f0c0c71dd53135f086da806db3ec2c6101bf95d4
LostWeb45 <kostik052005@gmail.com>
2025-01-17 13:26:11 +0300

    Кнопки

--- bot.js
+++ bot.js
@@ -1,16 +1,35 @@
-const { Bot } = require("grammy");
+const { Bot, InlineKeyboard } = require("grammy");
 
 const bot = new Bot("7956735247:AAFV_iB9H136sitrQvhPvTmOV2zIxHqvPqM");
 
 bot.command("start", (ctx) => {
+  const keyboard = new InlineKeyboard()
+    .text("Орел и решка", "orelandrescha")
+    .text("Камень, кожницы, бумага", "cmnplay")
+    .text("Угадай число", "changnumb");
   ctx.reply(
-    "Привет! Я на бот, а настоящий плейбойкарти. Напиши /help, чтобы узнать что я могу"
+    "Привет! Я Стив).\nНапиши /help, чтобы узнать что я умею. Или выбери игру: ",
+    {
+      reply_markup: keyboard,
+    }
   );
+});
+
+bot.on("callback_query:data", (ctx) => {
+  const data = ctx.callbackQuery.data;
+
+  if (data === "orelandrescha") {
+    ctx.reply("Начнем игру в Орел и решка!");
+  } else if (data === "cmnplay") {
+    ctx.reply("Начнем игру в Камень, ножница, бумага");
+  } else if (data === "changnumb") {
+    ctx.reply("Начнем игру Угадай число");
+  }
 });
 
 bot.command("help", (ctx) => {
   ctx.reply(
-    "/start - приветствие\n/help - помощь\n/eсho - повторить сообщение\n/joke - расскажи шутку"
+    "/start - приветствие\n/help - помощь\n/echo - повторить сообщение\n/joke - расскажи шутку"
   );
 });
 
@@ -20,8 +39,25 @@ bot.command("echo", (ctx) => {
 });
 
 bot.command("joke", (ctx) => {
-  ctx.reply("Не смешно.");
+  ctx.reply("Не буду.");
 });
+
+bot.api.setMyCommands([
+  { command: "start", description: "Запуск бота" },
+  { command: "help", description: "Получить список команд" },
+  { command: "echo", description: "Повторить сообщение" },
+  { command: "joke", description: "Рассказать шутку" },
+  { command: "cat", description: "Получить картинку кота" },
+]);
+// bot.command("cat", (ctx) => {
+//   const chatId = ctx.chat.id;
+//   if (ctx.text === "кот") {
+//     bot.sendPhoto(
+//       chatId,
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqCmqSFQgo-ixAhVspjiJX2XDwrcjvuqB8Hg&usqp=CAU"
+//     );
+//   }
+// });
 
 bot.start();
 console.log("Бот поднялся");
